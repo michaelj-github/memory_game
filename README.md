0e650a79006976f6881e8a932b3e958c388fb611
michaelj-github <mike@michaeljmurphy.com>
2022-10-12 17:16:03 -0400

    new version with playing cards and restart button

---
+++ README.md
@@ -0,0 +1,17 @@
+# Concentration
+
+- Originally done as the 'memory game' exercise for Springboard
+- Updating with playing card images, front and back, and a restart button
+- gets cards from the deckofcardsapi API
+
+## Solution notes
+
+uses:
+
+- HTML
+- CSS
+- JavaScript
+- Axios
+- deckofcardsapi API
+
+Open the index.html file in a browser
--- app.js
+++ app.js
@@ -1,36 +1,40 @@
-const gameContainer = document.getElementById("game");
-
-const COLORS = [
-  "red",
-  "#0057B7",
-  "green",
-  "#FFDD00",
-  "purple",
-  "red",
-  "#0057B7",
-  "green",
-  "#FFDD00",
-  "purple"
-];
-
+const gameContainer = document.getElementById("game-display");
+gameContainer.classList.add("game-container");
+const cardsForTheGame = [];
 let cardsTurned = 0; // keep track of how many cards have been turned over, can't be more than two
 const cardsArray = []; // array to keep track of the state of each card
 const cardsTurnedArray = []; // array to keep track of the state of each turned card
+const baseURL = "https://deckofcardsapi.com/api/deck/";
 
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want ot research more
-function shuffle(array) {
+const shuffle = "new/shuffle/";
+const draw = "draw/?count=6";
+
+async function getDeck() {
+  try {
+    const theDeck = await axios(`${baseURL}${shuffle}`);
+    let deckId = theDeck.data.deck_id;
+    let theCards = await axios(`${baseURL}/${deckId}/${draw}`);
+    const theDeckOfCards = theCards.data.cards;
+    for (e of theDeckOfCards) {
+      cardsForTheGame.push(e.images.png);
+    }
+    for (e of theDeckOfCards) {
+      cardsForTheGame.push(e.images.png);
+    }
+  } catch {
+    console.log("error getting deck");
+  }
+}
+
+function shuffleDeck(array) {
   let counter = array.length;
-
+  console.log("array.length = ", array.length);
   // While there are elements in the array
   while (counter > 0) {
     // Pick a random index
     let index = Math.floor(Math.random() * counter);
-
     // Decrease counter by 1
     counter--;
-
     // And swap the last element with it
     let temp = array[counter];
     array[counter] = array[index];
@@ -40,71 +44,102 @@ function shuffle(array) {
   return array;
 }
 
-let shuffledColors = shuffle(COLORS);
-
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
-function createDivsForColors(colorArray) {
+const createGameLayout = (cards) => {
   let theCard = 0;
-  for (let color of colorArray) {
+  for (e of cards) {
+    const newCardDiv = document.createElement("div");
+    newCardDiv.id = theCard;
+    newCardDiv.className = "display-card";
+    cardsArray[theCard] = false;
+    const cardImage = document.createElement("img");
+    cardImage.style.margin = "2px";
+    cardImage.src = "back.png";
+    newCardDiv.appendChild(cardImage);
+    newCardDiv.addEventListener("click", handelCardClick);
+    gameContainer.append(newCardDiv);
     theCard++;
-    // create a new div
-    const newDiv = document.createElement("div");
-
-    // give it a class attribute for the value we are looping over
-    newDiv.classList.add(color);
-    newDiv.id = theCard;
-    cardsArray[theCard - 1] = false;
-    // newDiv.classList.add(theCard);
-
-    // call a function handleCardClick when a div is clicked on
-    newDiv.addEventListener("click", handleCardClick);
-
-    // append the div to the element with an id of game
-    gameContainer.append(newDiv);
   }
-}
+  const restartButton = document.getElementById("newGame-button");
+  restartButton.addEventListener("click", handelRestartButtonClick);
+};
 
-// TODO: Implement this function!
-function handleCardClick(event) {
-  const divId = event.target.getAttribute('id');
-  if (cardsArray[divId - 1] == false) {
-    cardsTurned++
+const handelRestartButtonClick = () => {
+  console.log("button clicked");
+  cardsForTheGame.length = 0;
+  const cardsDisplayed = document.querySelectorAll(".display-card");
+  for (e of cardsDisplayed) {
+    // e.parentElement.remove();
+    e.remove();
+  }
+
+  playTheGame();
+};
+
+const handelCardClick = (event) => {
+  const divId = event.target.parentElement.getAttribute("id");
+  if (cardsArray[divId] === false) {
+    cardsTurned++;
     if (cardsTurned <= 2) {
       const divColor = event.target.classList.value;
-      // console.log(divColor, event.target, divId);
+      const divImage = cardsForTheGame[divId];
       document.getElementById(divId).style.backgroundColor = divColor;
-      cardsArray[divId - 1] = true; // this card is turned
+      const cardImage = document.getElementById(divId).firstChild;
+      let cardImageSrc = cardImage.getAttribute("src");
+      cardImageSrc = divImage;
+      cardImage.setAttribute("src", cardImageSrc);
+      cardsArray[divId] = true; // this card is turned
       cardsTurnedArray[cardsTurned] = divId;
-      if (cardsTurned === 2 && document.getElementById(cardsTurnedArray[1]).style.backgroundColor == document.getElementById(cardsTurnedArray[2]).style.backgroundColor) {
-        // console.log("match");
-        cardsTurned = 0;
-        let youWin = true;
-        for (let card of cardsArray) {
-          if (!card) {
-            youWin = false;
-            // console.log(youWin, card, cardsArray)
+      if (cardsTurned === 2) {
+        const cardImage1 = document.getElementById(
+          cardsTurnedArray[1]
+        ).firstChild;
+        const cardImageSrc1 = cardImage1.getAttribute("src");
+        const cardImage2 = document.getElementById(
+          cardsTurnedArray[2]
+        ).firstChild;
+        const cardImageSrc2 = cardImage2.getAttribute("src");
+        if (cardImageSrc1 === cardImageSrc2) {
+          cardsTurned = 0;
+          let youWin = true;
+          for (let card of cardsArray) {
+            if (!card) {
+              youWin = false;
+            }
           }
+          if (youWin) {
+          }
+        } else {
+          // wait 1 second
+          setTimeout(function () {
+            const cardImage1 = document.getElementById(
+              cardsTurnedArray[1]
+            ).firstChild;
+            let cardImageSrc1 = cardImage1.getAttribute("src");
+            cardImageSrc1 = divImage;
+            cardImage1.setAttribute("src", "back.png");
+
+            const cardImage2 = document.getElementById(
+              cardsTurnedArray[2]
+            ).firstChild;
+            let cardImageSrc2 = cardImage2.getAttribute("src");
+            cardImageSrc2 = divImage;
+            cardImage2.setAttribute("src", "back.png");
+            cardsArray[cardsTurnedArray[1]] = false; // this card is turned
+            cardsArray[cardsTurnedArray[2]] = false; // this card is turned
+            cardsTurned = 0;
+          }, 1000);
         }
-        if (youWin) {
-          // console.log("Game Over! Click Refresh to replay.");
-          // alert("Game Over! Click OK and then click Refresh to replay.");
-        }
-      }
-      else if (cardsTurned === 2 ) {
-            // wait 1 second
-            setTimeout(function(){
-              document.getElementById(cardsTurnedArray[1]).style.backgroundColor = "white";
-              document.getElementById(cardsTurnedArray[2]).style.backgroundColor = "white";
-              cardsArray[cardsTurnedArray[1] - 1] = false; // this card is turned
-              cardsArray[cardsTurnedArray[2] - 1] = false; // this card is turned
-              cardsTurned = 0;
-            }, 1000)
       }
     }
   } //   if cardsTurned <= 2
+};
+
+async function playTheGame() {
+  console.log("Play the game");
+  await getDeck();
+  console.log("cardsForTheGame = ", cardsForTheGame);
+  const shuffledCards = shuffleDeck(cardsForTheGame);
+  createGameLayout(shuffledCards);
 }
 
-// when the DOM loads
-createDivsForColors(shuffledColors);
+playTheGame();
